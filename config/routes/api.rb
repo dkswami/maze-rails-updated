25d5c4b6780914af673ba59444db545b32596c4c
dkswami <dkswami1820@gmail.com>
2022-09-29 18:12:18 +0530

    api tested and working successfully

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -12,10 +12,10 @@ namespace :api do
   end
 end
 
-# scope :api do
-#   scope :v1 do
-#     use_doorkeeper do
-#       skip_controllers :authorizations, :applications, :authorized_applications
-#     end
-#   end
-# end
+scope :api do
+  scope :v1 do
+    use_doorkeeper do
+      skip_controllers :authorizations, :applications, :authorized_applications
+    end
+  end
+end
