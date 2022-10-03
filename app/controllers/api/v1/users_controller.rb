2e30aed750fd8c97db221082668ba3002790814f
dkswami <dkswami1820@gmail.com>
2022-10-03 15:08:05 +0530

    rendering new fields in /users/me api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,6 +14,9 @@ module Api
           else
             render json: {
               id: @current_user.id,
+              first_name: @current_user.first_name,
+              last_name: @current_user.last_name,
+              phone_number: @current_user.phone_number,
               email: @current_user.email,
               role: @current_user.role,
               created_at: @current_user.created_at.iso8601
